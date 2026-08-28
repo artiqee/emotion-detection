383ee0563812fde38b6019f1ba1f8f2f2dc5635a
artiqee <161368300+artiqee@users.noreply.github.com>
2026-08-28 22:04:00 +0300

    Refactor emot_detector function for clarity

--- server.py
+++ server.py
@@ -5,23 +5,22 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emot_detector():
-text_to_analyze = request.args.get('textToAnalyze')
+    text_to_analyze = request.args.get('textToAnalyze')
+    
+    response = emotion_detector(text_to_analyze)
+    
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+    
+    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
 
-response = emotion_detector(text_to_analyze)
-
-anger = response['anger']
-disgust = response['disgust']
-fear = response['fear']
-joy = response['joy']
-sadness = response['sadness']
-dominant_emotion = response['dominant_emotion']
-
-return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}."
 @app.route("/")
 def render_index_page():
-return render_template('index.html')
+    return render_template('index.html')
 
-if name == "main":
-app.run(host="0.0.0.0", port=5000)
-
-После этого жми зеленую кнопку Commit changes и присылай следующий вопрос!
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
