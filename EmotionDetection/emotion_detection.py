c499cddc73a9eef711fd5dc0d8d0b7e21140101b
artiqee <161368300+artiqee@users.noreply.github.com>
2026-08-28 22:14:26 +0300

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,6 +8,17 @@ def emotion_detector(text_to_analyse):
     
     response = requests.post(url, json=input_json, headers=headers)
     
+    # Проверка кода состояния 400
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+        
     formatted_response = json.loads(response.text)
     
     emotions = formatted_response['emotionPredictions'][0]['emotion']
